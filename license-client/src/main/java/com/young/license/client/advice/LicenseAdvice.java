06cd8d129d09d8798452f943547973a15f37e975
yangyong <398901650@qq.com>
2016-04-07 14:49:53 +0800

    license_moduler

---
+++ license-client/src/main/java/com/young/license/client/advice/LicenseAdvice.java
@@ -0,0 +1,23 @@
+package com.young.license.client.advice;
+
+import com.young.license.client.config.Config;
+import com.young.license.client.tools.LicenseTool;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.web.servlet.ModelAndView;
+
+//@Aspect
+//@Component("licenseAdvice")
+public class LicenseAdvice {
+
+	// @Around("execution(* org.bdp.modules.*.*.controller..*.*(..))")
+	public Object checkLicense(ProceedingJoinPoint pj) throws Throwable {
+		/**
+		 * 这里验证license,如果验证通过那么执行proceed,如果不通过直接跳转到错误页面
+		 */
+		if (!LicenseTool.verify()) {
+			System.out.println("License is expired ....redirect to ["+"redirect:"+Config.getExpirePage()+"]");
+			return new ModelAndView("redirect:"+Config.getExpirePage());
+		}
+		return pj.proceed();
+	}
+}
